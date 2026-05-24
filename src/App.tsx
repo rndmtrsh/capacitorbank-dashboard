16214976a56e9097b991183051eef706010d03d8
rndmtrsh <t.maulanaakmal@gmail.com>
2026-05-25 03:44:56 +0700

    fix:alot

--- src/App.tsx
+++ src/App.tsx
@@ -31,18 +31,18 @@ type PhaseState = {
 
 const PHASES: PhaseKey[] = ['R', 'S', 'T']
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.FARHAN_FIREBASE_API_KEY,
+  authDomain: import.meta.env.FARHAN_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.FARHAN_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.FARHAN_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FARHAN_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FARHAN_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FARHAN_FIREBASE_APP_ID,
+  measurementId: import.meta.env.FARHAN_FIREBASE_MEASUREMENT_ID,
 }
 
 const firebaseApp = initializeApp(firebaseConfig)
-if (import.meta.env.VITE_FIREBASE_MEASUREMENT_ID) {
+if (import.meta.env.FARHAN_FIREBASE_MEASUREMENT_ID) {
   getAnalytics(firebaseApp)
 }
 const realtimeDb = getDatabase(firebaseApp)
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  envPrefix: ['FARHAN_'],
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
   ],
 })
